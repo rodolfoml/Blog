53beb2afe4af8a4c76a80c02aebadce957a7aa30
Rodolfo Megiato de Lima <rodolfomegiato@gmail.com>
2019-08-19 14:13:40 -0300

    Fixed post controller identation

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,51 +1,50 @@
+# frozen_string_literal: true
+
 class PostsController < ApplicationController
+  before_action :find_post, only: %i[show edit update destroy]
 
-        before_action :find_post, only: [:show, :edit, :update, :destroy]
+  def index
+    @posts = Post.all
+  end
 
-        def index
-            @posts = Post.all
-        end
+  def show; end
 
-        def show
-        end
-        
-        def new
-            @post = Post.new
-        end
+  def new
+    @post = Post.new
+  end
 
-        def create
-            @post = Post.new(post_params)
+  def create
+    @post = Post.new(post_params)
 
-            if @post.save
-                redirect_to @post, notice: "The post was created!"
-            else
-                render 'new'
-            end
-        end
-        
-        def edit
-        end
-        
-        def update
-            if @post.update(post_params)
-                redirect_to @post, notice: "Update succesful"
-            else
-                render 'edit'
-            end
-        end
-        
-        def destroy
-            @post.destroy
-            redirect_to root_path, notice: "Post destroyed"
-        end
+    if @post.save
+      redirect_to @post, notice: 'The post was created!'
+    else
+      render 'new'
+    end
+  end
 
-    private
+  def edit; end
 
-        def post_params
-            params.require(:post).permit(:title, :content)
-        end
+  def update
+    if @post.update(post_params)
+      redirect_to @post, notice: 'Update succesful'
+    else
+      render 'edit'
+    end
+  end
 
-        def find_post
-            @post = Post.find(params[:id])
-        end
+  def destroy
+    @post.destroy
+    redirect_to root_path, notice: 'Post destroyed'
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :content)
+  end
+
+  def find_post
+    @post = Post.find(params[:id])
+  end
 end
